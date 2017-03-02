681db1bad8ea5606ca33575554d537506f004f26
daveaugerinos <dave.augerinos@gmail.com>
2017-03-02 08:45:17 -0800

    Sort with title

--- Every-Do-It-Again/MasterViewController.m
+++ Every-Do-It-Again/MasterViewController.m
@@ -140,7 +140,7 @@
     [fetchRequest setFetchBatchSize:20];
     
     // Edit the sort key as appropriate.
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:NO];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES];
 
     [fetchRequest setSortDescriptors:@[sortDescriptor]];
     
